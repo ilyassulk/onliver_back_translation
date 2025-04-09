bc9d5a03724bf34ee8b5edd59fa7ef00bc3c0e82
ilyassulk <20044002ilya@gmail.com>
2025-04-09 12:19:23 +0300

    Initial commit

---
+++ translation_streamer/src/test/java/ru/onliver/film_streamer/FilmStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.onliver.film_streamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmStreamerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
