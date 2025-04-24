ec76c04e644f05b172f63abdb2d11002e41b94b3
ilyassulk <20044002ilya@gmail.com>
2025-04-24 23:17:11 +0300

    init translation manager

---
+++ translation_manager/src/main/java/ru/onliver/translation_manager/TranslationManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.onliver.translation_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationManagerApplication.class, args);
+    }
+
+}
