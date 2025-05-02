c1ec9483ee606e0d135212682316f053ddfc94c6
ilyassulk <20044002ilya@gmail.com>
2025-05-02 15:01:56 +0300

    big test 2

---
+++ translation_gc/src/main/java/ru/onliver/translation_gc/TranslationGcApplication.java
@@ -0,0 +1,13 @@
+package ru.onliver.translation_gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationGcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationGcApplication.class, args);
+    }
+
+}
---
+++ translation_gc/src/test/java/ru/onliver/translation_gc/TranslationGcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.onliver.translation_gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationGcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
